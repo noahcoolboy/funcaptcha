09f314657c269d87901ec61ebd155307583337ad
noahcoolboy <noah.nuelandt@gmail.com>
2022-09-09 21:42:28 +0200

    Hack

--- test/roblox.js
+++ test/roblox.js
@@ -22,46 +22,47 @@ undici.request("https://auth.roblox.com/v2/signup", {
         })
     })
     const body = await res2.body.json()
-
-    const fieldData = body.errors[0].fieldData.split(",")
-    const token = await funcaptcha.getToken({
-        pkey: "A2A14B1D-1AF3-C791-9BBC-EE33CC7A0A6F",
-        surl: "https://roblox-api.arkoselabs.com",
-        data: {
-            "blob": fieldData[1],
-        },
-        headers: {
-            "User-Agent": USER_AGENT,
-        },
-        site: "https://www.roblox.com",
-    })
-
-    let session = new funcaptcha.Session(token, {
-        userAgent: USER_AGENT,
-    })
-    let challenge = await session.getChallenge()
-
-    console.log("Signup", challenge.data.game_data.game_variant, challenge.data.game_data.waves)
-
-    if (
-        challenge.data.game_data.game_variant && (
-            challenge.data.game_data.game_variant.startsWith("dice_") ||
-            challenge.data.game_data.game_variant.startsWith("dart") ||
-            challenge.data.game_data.game_variant.startsWith("context-") ||
-            [
-                "shadow-icons",
-                "penguins",
-                "shadows",
-                "mismatched-jigsaw",
-                "stairs_walking",
-                "reflection",
-            ].includes(challenge.data.game_data.game_variant)
-        )
-    ) {
-        console.log("Signup", "Test failed :(")
-    } else {
-        console.log("Singup", "Test passed!")
-    }
+    setTimeout(async () => {
+        const fieldData = body.errors[0].fieldData.split(",")
+        const token = await funcaptcha.getToken({
+            pkey: "A2A14B1D-1AF3-C791-9BBC-EE33CC7A0A6F",
+            surl: "https://roblox-api.arkoselabs.com",
+            data: {
+                "blob": fieldData[1],
+            },
+            headers: {
+                "User-Agent": USER_AGENT,
+            },
+            site: "https://www.roblox.com",
+        })
+    
+        let session = new funcaptcha.Session(token, {
+            userAgent: USER_AGENT,
+        })
+        let challenge = await session.getChallenge()
+    
+        console.log("Signup", challenge.data.game_data.game_variant, challenge.data.game_data.waves)
+    
+        if (
+            challenge.data.game_data.game_variant && (
+                challenge.data.game_data.game_variant.startsWith("dice_") ||
+                challenge.data.game_data.game_variant.startsWith("dart") ||
+                challenge.data.game_data.game_variant.startsWith("context-") ||
+                [
+                    "shadow-icons",
+                    "penguins",
+                    "shadows",
+                    "mismatched-jigsaw",
+                    "stairs_walking",
+                    "reflection",
+                ].includes(challenge.data.game_data.game_variant)
+            )
+        ) {
+            console.log("Signup", "Test failed :(")
+        } else {
+            console.log("Singup", "Test passed!")
+        }
+    }, 2500)
 })
 
 undici.request("https://auth.roblox.com/v2/login", {
@@ -123,7 +124,7 @@ undici.request("https://auth.roblox.com/v2/login", {
         } else {
             console.log("Login", "Test passed!")
         }
-    }, 1000);
+    }, 2500);
 
 })
 
