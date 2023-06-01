5bf2c036455a0998d4abe389c18fc4450a2d6976
yourmomisgay69420 <101905422+AlexandraBaker@users.noreply.github.com>
2023-06-01 23:18:51 +0300

    Update default UA

--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,7 @@ interface TimestampData {
 }
 
 const DEFAULT_USER_AGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
 
 let apiBreakers = {
     default: (c) => {
